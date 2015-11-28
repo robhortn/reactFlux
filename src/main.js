fb0cfde9cea6f3a108d4b752d6a458194649c99f
Rob Horton <robhorton@outlook.com>
2015-11-28 15:48:56 -0600

    Added config file.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+$ = jQuery = require('jquery');	// This is saying there are two ways to reference jquery .. by 'jquery' AND by the '$'.
+
+//test = 1;
+
+var App = console.log('Hello world from Browserify');
+
+module.exports = App;
+
