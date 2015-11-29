7b892b9463d0151139384b4dde495dc265f1405e
Rob Horton <robhorton@outlook.com>
2015-11-28 20:55:51 -0600

    Added really basic routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
 $ = jQuery = require('jquery');	// This is saying there are two ways to reference jquery .. by 'jquery' AND by the '$'.
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-React.render(<Home />, document.getElementById('app'));
+(function(win){
+
+	"use strict";
+	
+	var App = React.createClass({
+		render: function(){
+			var Child;
+
+			switch(this.props.route){
+				case 'about' : Child = About; break;
+				default: Child = Home;
+			}
+
+			return (
+				<div>
+					<Child/>
+				</div>
+			);
+		}
+	});
+
+	function render(){
+		var route = win.location.hash.substr(1);
+		React.render(<App route={route} />, document.getElementById('app'));
+	}
+
+	win.addEventListener('hashchange', render);
+
+	render();
+
+	//React.render(<Home />, document.getElementById('app'));
+
+})(window);
